ced1c9b127fdf53156ac2a2acb93329f2bcc72b6
Muhammad Usman <muhammad@usmanity.com>
2015-03-16 22:03:08 -0700

    commenting some stuff out

--- src/override/tab.js
+++ src/override/tab.js
@@ -53,10 +53,9 @@ var displayFeed = function(feed){
       }
   }
   getSettings().then(function(settings){
-    console.log(settings);
     var loopSetting = settings.loop === 'true';
     $("video").attr('loop', loopSetting);
-  })
+  });
   {{timer_end}}
 };
 
@@ -71,6 +70,7 @@ function pause(){
   $(this).find('.play').removeClass('hidden');
   $(this).unbind('click').click(play);
 }
+
 function getSettings(){
   return new Promise(function (fulfill, reject){
     chrome.storage.local.get('options', function(cb){
@@ -81,8 +81,8 @@ function getSettings(){
       fulfill(options);
     });
   })
-
 }
+
 if (getPage() === 'tab'){
   $("#authLink").attr("href", AUTH_URL)
   getAuthInterval = window.setInterval(function(){
