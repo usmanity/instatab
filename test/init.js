40c4056d2440f13ff6efeac06afab24b10485cb4
Muhammad Usman <muhammad@usmanity.com>
2015-07-12 20:06:11 -0700

    travis yml file added

--- test/init.js
+++ test/init.js
@@ -1,2 +1,16 @@
 var mocha = require("mocha");
 var assert = require("chai").assert;
+var config = require("../config.json");
+
+describe("constants", function() {
+  describe("client_id", function() {
+    it("should have a client_id", function() {
+      assert.ok("client_id", config.dev.client_id);
+    });
+  });
+  describe("redirect_uri", function(){
+    it("should have a redirect_uri", function(){
+      assert.ok("redirect_uri", config.dev.redirect_uri)
+    })
+  });
+});
